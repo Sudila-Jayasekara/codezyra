03f458491dcedca512250b86c6ee0f1d99f33dd5
Sudila Jayasekara <mpsjayasekara@gmail.com>
2024-10-23 16:31:17 +0530

    add typing text component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import TypingText from './components/TypingText'
+import HomePage from './pages/HomePage'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    <div className='flex w-full h-screen items-center justify-center bg-gray-500'>
-      <h1 className='text-3xl font-bold text-white'>Welcome to CodeZyra</h1>
-    </div>
+    <HomePage/>
     </>
   )
 }
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import TypingText from '../components/TypingText';
+
+const HomePage = () => {
+  return (
+    <div className='w-full h-screen flex items-center justify-center bg-gray-800'>
+      <h1 className='text-3xl font-semibold text-white'>
+        <TypingText message="Welcome to Codezyra ..."/>
+      </h1>
+    </div>
+  );
+};
+
+export default HomePage;

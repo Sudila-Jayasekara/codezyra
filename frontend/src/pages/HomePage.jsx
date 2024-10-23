a20d42e70a7df35a2208f3052f4503d22fd7c607
Sudila Jayasekara <mpsjayasekara@gmail.com>
2024-10-23 17:53:59 +0530

    update with reac-typed

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,28 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import TypingText from '../components/reuse/TypingText';
+import React from "react";
+import { ReactTyped } from "react-typed";
+
 
 const HomePage = () => {
-  const [showNextMessage, setShowNextMessage] = useState(false);
-
-  useEffect(() => {
-    // Delay the display of the second message
-    const timer = setTimeout(() => {
-      setShowNextMessage(true);
-    }, 3000); // Adjust this duration as needed (3000 ms = 3 seconds)
-
-    return () => clearTimeout(timer); // Clean up the timer
-  }, []);
-
   return (
     <div className='w-full h-screen flex flex-col items-center justify-center bg-gray-800'>
-      <h1 className='text-3xl font-semibold text-white'>
-        <TypingText message="www.codezyra.com" />
+      <h1 className='text-5xl font-semibold text-white'>
+        <ReactTyped
+          strings={["www.codezyra.com", "Welcome Dhanuthi"]}
+          typeSpeed={100}
+          backSpeed={50}
+          backDelay={1000}
+          showCursor={true}
+          loop={false}
+        />
       </h1>
-      {showNextMessage && (
-        <p className='text-xl font-semibold text-white mt-4'>
-          <TypingText message="Hi Dhanuthi" />
-        </p>
-      )}
     </div>
   );
 };

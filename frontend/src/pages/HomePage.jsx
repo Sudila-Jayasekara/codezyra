53fd6b97262ed3263a9a63d1fe3ea14f442f1a66
Sudila Jayasekara <mpsjayasekara@gmail.com>
2024-10-23 16:48:05 +0530

    Update HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -5,7 +5,7 @@ const HomePage = () => {
   return (
     <div className='w-full h-screen flex items-center justify-center bg-gray-800'>
       <h1 className='text-3xl font-semibold text-white'>
-        <TypingText message="www.codezyra.com"/>
+        <TypingText message="welcome srilanaka"/>
       </h1>
     </div>
   );

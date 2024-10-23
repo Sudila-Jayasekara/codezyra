53d84cd73aeddf4370e0ef771f721971a313df5c
Sudila Jayasekara <mpsjayasekara@gmail.com>
2024-10-23 17:03:42 +0530

    Update TypingText.jsx

--- frontend/src/components/reuse/TypingText.jsx
+++ frontend/src/components/reuse/TypingText.jsx
@@ -1,31 +1,3 @@
-// import React, { useEffect, useState } from 'react';
-
-// const TypingText = ({ message }) => {
-//   const [text, setText] = useState('');
-//   const speed = 100;
-
-//   useEffect(() => {
-//     let index = 0;
-//     let frameId;
-
-//     const type = () => {
-//       if (index < message.length) {
-//         setText((prev) => prev + message.charAt(index));
-//         index++;
-//         frameId = requestAnimationFrame(() => setTimeout(type, speed));
-//       }
-//     };
-
-//     type();
-
-//     return () => cancelAnimationFrame(frameId);
-//   }, [message]);
-
-//   return <span>{text}</span>;
-// };
-
-// export default TypingText;
-
 import React, { useEffect, useState } from 'react';
 
 const TypingText = ({ message }) => {

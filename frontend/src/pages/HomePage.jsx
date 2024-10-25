0a769c167b59f73bb7b367fb003aadb7b6f30cda
Sudila Jayasekara <mpsjayasekara@gmail.com>
2024-10-25 16:57:26 +0530

    update homepage with vectors

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,26 +1,52 @@
 import React from 'react';
-import user from '../assets/user.png';
+import amico from '../assets/home_page/Programming-amico.svg';
+import bro from '../assets/home_page/Programming-bro.svg';
+import cuate from '../assets/home_page/Programming-cuate.svg';
+import pana from '../assets/home_page/Programming-pana.svg';
+import rafiki from '../assets/home_page/Programming-rafiki.svg';
+
+const Container = ({ bgColor, children }) => {
+  return (
+    <div className={`min-h-screen flex flex-col md:flex-row pl-4 pr-4 md:pl-32 md:pr-32 ${bgColor}`}>
+      {children}
+    </div>
+  );
+};
+
 const HomePage = () => {
   return (
-    <div className='mt-16 pt-6 max-w-screen-2xl mx-auto'>
+    <div className="mt-10 pt-6 max-w-screen-2xl mx-auto">
 
-      <div className='h-screen flex flex-col md:flex-row pl-4 pr-4 md:pl-32 md:pr-32'>
-          <img src={user} alt='User' className='w-40 h-40 object-cover' />
-      </div>
-      
-      <div className='h-screen pl-4 pr-4 md:pl-32 md:pr-32 bg-white'>
-        <div>
-          2nd container
+      <Container bgColor="bg-backgroundLightGray">
+        <div className="flex-1 flex justify-center md:justify-start items-center ">
+          <h1 className="text-4xl font-bold text-black">"About CodeZyra"</h1>
+        </div>
+        <div className="flex-1 flex justify-center items-center">
+          <img src={amico} alt="amico" className="w-full max-w-md md:max-w-lg" />
+        </div>
+      </Container>
 
+      <Container bgColor="bg-white">
+        <div className="flex-1 flex justify-center md:justify-start items-center ">
+          <h1 className="text-4xl font-bold text-black">"Our Mission"</h1>
         </div>
-      </div>
-      <div className='h-screen pl-4 pr-4 md:pl-32 md:pr-32'>
-        <div>
-          3rd container
+        <div className="flex-1 flex justify-center items-center">
+          <img src={bro} alt="bro" className="w-full max-w-md md:max-w-lg" />
         </div>
-      </div>
+      </Container>
+
+      <Container bgColor="bg-blue-500">
+        <div className="flex-1 flex justify-center md:justify-start items-center ">
+          <h1 className="text-4xl font-bold text-white">"Join Us"</h1>
+        </div>
+        <div className="flex-1 flex justify-center items-center">
+          <img src={cuate} alt="cuate" className="w-full max-w-md md:max-w-lg" />
+        </div>
+      </Container>
+
     </div>
-  )
+  );
 };
+
 
 export default HomePage;

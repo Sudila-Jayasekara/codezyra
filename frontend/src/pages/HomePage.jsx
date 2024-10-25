6b31af9492d91ad03d61bcc2e4aafbfea70f5ede
Sudila Jayasekara <mpsjayasekara@gmail.com>
2024-10-25 06:49:42 +0530

    fix header

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,26 +1,24 @@
 import React from 'react';
-
+import user from '../assets/user.png';
 const HomePage = () => {
   return (
-    <div className='mt-40 max-w-screen-2xl mx-auto'>
-      <div className='h-screen pl-32 pr-32'>
-        <div>
-          1st container
-        </div>
-      </div>
+    <div className='mt-16 pt-6 max-w-screen-2xl mx-auto'>
 
-      <div className='h-screen pl-32 pr-32 bg-white'>
+      <div className='h-screen flex flex-col md:flex-row pl-4 pr-4 md:pl-32 md:pr-32'>
+          <img src={user} alt='User' className='w-40 h-40 object-cover' />
+      </div>
+      
+      <div className='h-screen pl-4 pr-4 md:pl-32 md:pr-32 bg-white'>
         <div>
           2nd container
+
         </div>
       </div>
-
-      <div className='h-screen pl-32 pr-3'>
+      <div className='h-screen pl-4 pr-4 md:pl-32 md:pr-32'>
         <div>
           3rd container
         </div>
       </div>
-
     </div>
   )
 };

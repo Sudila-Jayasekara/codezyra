26485ddd8d918e56f590d5438a8c7a46d8973e8e
Sudila Jayasekara <mpsjayasekara@gmail.com>
2024-10-25 19:09:02 +0530

    Update HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -14,33 +14,49 @@ const HomePage = () => {
     <div className="mt-10 pt-6 max-w-screen-2xl mx-auto">
 
       <Container bgColor="bg-backgroundLightGray">
-        <div className="flex-1 flex justify-center items-end md:items-center">
+        <div className="flex-1 flex justify-around items-end md:items-center">
           <h1 className="text-4xl font-bold text-black">About CodeZyra</h1>
         </div>
-        <div className="flex-1 flex items-center justify-center">
+        <div className="flex-1 flex justify-around items-end md:items-center">
           <img src={amico} alt="amico" className="w-full max-w-md md:max-w-lg" />
         </div>
       </Container>
 
-
-      <Container bgColor="bg-white">
-        <div className="flex-1 flex justify-center items-end md:items-center ">
-          <h1 className="text-4xl font-bold text-black">Our Mission</h1>
+      <Container bgColor="bg-blue-500">
+        <div className="flex-1 flex justify-around items-end md:items-center ">
+          <h1 className="text-4xl font-bold text-white">Join Us</h1>
         </div>
-        <div className="flex-1 flex justify-center items-center">
+        <div className="flex-1 flex justify-around items-end md:items-center">
           <img src={bro} alt="bro" className="w-full max-w-md md:max-w-lg" />
         </div>
       </Container>
+      <Container bgColor="bg-backgroundLightGray">
+  <div className="flex-1 flex flex-col  justify-around items-center">
+    <h1 className="text-4xl font-bold text-black">Our Packages</h1>
 
-      <Container bgColor="bg-blue-500">
-        <div className="flex-1 flex justify-center items-end md:items-center ">
-          <h1 className="text-4xl font-bold text-white">Join Us</h1>
+    <div className="w-full flex flex-col space-y-10 px-6 py-6 md:flex-row md:space-y-0 md:space-x-10 md:justify-around">
+      <div className="w-full flex justify-center p-8  md:w-1/3 h-96 rounded-lg shadow-lg bg-white ">
+      <h1 className='text-2xl font-bold'>Package 1</h1>
+      </div>
+      <div className="w-full flex justify-center p-8  md:w-1/3 h-96 rounded-lg shadow-lg bg-white ">
+      <h1 className='text-2xl font-bold'>Package 2</h1>
+      </div>
+      <div className="w-full flex justify-center p-8  md:w-1/3 h-96 rounded-lg shadow-lg bg-white ">
+      <h1 className='text-2xl font-bold'>Package 3</h1>
+      </div>
+    </div>
+  </div>
+</Container>
+
+
+      <Container bgColor="bg-white">
+        <div className="flex-1 flex justify-around items-end md:items-center">
+          <h1 className="text-4xl font-bold text-black">Contact Us</h1>
         </div>
-        <div className="flex-1 flex justify-center items-center">
-          <img src={cuate} alt="cuate" className="w-full max-w-md md:max-w-lg" />
+        <div className="flex-1 flex justify-around items-end md:items-center">
+          <img src={cuate} alt="amico" className="w-full max-w-md md:max-w-lg" />
         </div>
       </Container>
-
     </div>
   );
 };
